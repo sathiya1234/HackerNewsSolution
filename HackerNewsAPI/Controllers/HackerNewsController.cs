d014ab7929328255a3d70d1152fc7bd6f6ca3408
Sathiyaseelan Anandan <SathiyaseelanA@clarium.tech>
2025-04-23 17:25:16 +0530

    Add project files.

---
+++ HackerNewsAPI/Controllers/HackerNewsController.cs
@@ -0,0 +1,37 @@
+﻿using HackerNewsAPI.Models;
+using HackerNewsAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackerNewsAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HackerNewsController : ControllerBase
+    {
+        private readonly IHackerNewsService _hackerNewsService;
+
+        public HackerNewsController(IHackerNewsService hackerNewsService)
+        {
+            _hackerNewsService = hackerNewsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StoryModel>>> GetNewestStories(int page, int pageSize)
+        {
+            var stories = await _hackerNewsService.GetNewestStories(page, pageSize);
+            return Ok(stories);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StoryModel>>> SearchStories(string term)
+        {
+            if (string.IsNullOrWhiteSpace(term))
+            {
+                return BadRequest("Search term is required");
+            }
+
+            var stories = await _hackerNewsService.SearchStories(term);
+            return Ok(stories);
+        }
+    }
+}
---
+++ HackerNewsAPI/Program.cs
@@ -0,0 +1,47 @@
+using HackerNewsAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularDevClient",
+        builder =>
+        {
+            builder
+                .WithOrigins("http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+builder.Services.AddHttpClient();
+builder.Services.AddMemoryCache();
+builder.Services.AddScoped<IHackerNewsService, HackerNewsService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Use CORS policy
+app.UseCors("AllowAngularDevClient");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
